43fab36ba3552436331bcbe4d0a6eae4ce573f19
Jarno Hänninen <jakahann@student.jyu.fi>
2018-07-23 17:14:10 +0300

    my website initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,103 @@
+const pkg = require('./package')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Jarno Hänninen',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'My cool website' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css?family=Open+Sans" }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#FF0000', duration: 5000 },
+  loadingIndicator: {
+    name: 'circle',
+    color: '#fa923f'
+  },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~assets/styles/main.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~plugins/core-components.js',
+    '~plugins/date-filter.js'
+
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios',
+  ],
+  axios: {
+    baseURL: process.env.BASE_URL || 'https://nuxt-blog-6f90c.firebaseio.com',
+    credentials: false
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+
+    }
+  },
+
+  env: {
+    baseUrl: process.env.BASE_URL || 'https://nuxt-blog-6f90c.firebaseio.com',
+    fbAPIKey: 'AIzaSyDfJ0gDCj8lShpuD2Dp3K5i9HW2KimC_QQ'
+  },
+  transition: {
+    name: 'fade',
+    mode: 'out-in'
+  },
+  // router: {
+  //   middleware: 'log'
+  // }
+  serverMiddleware: [
+    bodyParser.json(),
+    '~/api'
+
+  ],
+  generate: {
+    routes: function () {
+      return axios.get('https://nuxt-blog-6f90c.firebaseio.com/posts.json')
+        .then(res => {
+          const routes = []
+          for (const key in res.data) {
+            routes.push({
+              route: '/posts/' + key,
+              payload: { postData: res.data[key] }
+            })
+          }
+          return routes
+        })
+
+    }
+  }
+}
