8030aed712607f59cd45e1e7b1c823141e5ee00b
Jarno Hänninen <jakahann@student.jyu.fi>
2018-11-30 16:56:34 +0200

    front page responsivness

--- pages/index.vue
+++ pages/index.vue
@@ -1,133 +1,97 @@
 <template>
   <div class="home-page">
-    <!-- <section class="intro">
-      <h1> Tervetuloa sivuilleni</h1>
-    </section> -->
     <div class="content">
       <h1>Jarno Hänninen</h1>
-        <h2>Software Developer</h2>
-       
-    </div></div>
-
+      <h2>Software Developer</h2>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   computed: {
     loadedPosts() {
-      return this.$store.getters.loadedPosts
+      return this.$store.getters.loadedPosts;
     }
   }
-
 };
 </script>
 
 <style scoped>
-
 @home-page-font {
   font-family: homePageFont;
-  src: ("~/assets/fonts/AllerDisplay.ttf")
-}
-h1 {
-  position: relative;
-  left: 16%;
-  font-family: homePageFont;
-  font-size: 4.5em;
-  color: white;
-  margin: 0px;
-}
-
-
-h2{
-  position: relative;
-  font-family: homePageFont;
-  left: 22%;
-  font-size: 3.0em;
-  color: white;
-  margin: 0px;  
-  opacity: 0.2;
+  src: ("~/assets/fonts/AllerDisplay.ttf");
 }
 
 .home-page {
-  min-height: 100vh;
+  position: fixed;
   background-image: url("~assets/images/main-back.jpg");
+  width: 100%;
+  height: 100%;
   background-position: center;
   background-size: cover;
- }
+  text-align: center;
+  
+}
 
 .content {
   position: relative;
-  top: 100px;
+  right: 2%;
+  top: 10%;
+  
 }
 
-.content p {
-  text-align: justify;
-  margin: 0 auto;
-}
-
-@media (min-width: 768px) {
-  .content {
-    width: 800px;
-    margin: auto;
-  }
-}
-
-
-.intro h1 {
-  position: relative;
-  margin-top: 0px;
-  color: black;
-  top: 15px;
-  left: 25%;
-  background-color: rgb(211, 211, 211);
-  opacity: 0.9;
-  padding: 10px;
-  border-radius: 10px;
-  box-shadow: 3px 3px 3px black;
-  box-sizing: border-box;
-  border: 1px solid black;
-  text-align: center;
-  width: 50%;
-}
-
-/* .down {
-  height: 70vh;
-  background-image: url("~assets/images/main-background.jpg");
-  background-size: cover;
-  } */
-
-/* .intro {
-  height: 300px;
-  position: relative;
-  padding: 30px;
-  background-image: url("~assets/images/main-image.jpg");
-  background-position: center;
-  background-size: cover;
-  box-sizing: border-box;
-}
-
-@media (min-width: 768px) {
-  .intro h1 {
-    font-size: 2rem;
-  }
-}
-
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-}
-
+h1,
 h2 {
-  text-align: center;
-} 
+  position: relative;
+  /* top: 10%; */
+  font-family: homePageFont;
+  font-size: 450%;
+  color: white;
+  margin: 0px;
+}
+h2 {
+  font-size: 300%;
+  opacity: 0.2;
+}
 
-a {
-  margin-top: 30px;
-  border: 3px solid grey;
-  padding: 10px;
+/* @media screen and (max-width: 1900px){
+  .content {
+    margin-top: 5vw;
+  }
 } */
+
+@media screen and (max-width: 500px) {
+  h1,
+  h2 {
+    display: none;
+  }
+}
+
+@media screen and (min-width: 2700px) {
+  
+  h2 {
+    display: none;
+  }
+}
+
+
+@media screen and (max-height: 900px ) {
+  h2 {
+    display: none;
+  }
+}
+
+@media screen and (max-height: 750px ) {
+  .content {
+    top: 1vh;
+  }
+}
+@media screen and (max-height: 600px ) {
+  .content {
+    display: none;
+  }
+}
+
+
 </style>
