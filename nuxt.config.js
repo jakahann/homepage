9756275ba193c329862f2ddb057c977f04f1c36b
jakahann <jakahann@student.jyu.fi>
2018-12-02 22:21:20 +0200

    testing background styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'My cool website' }
+      { hid: 'description', name: 'description', content: 'my-website' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home-page">
-   
+    <div class="content">
+      <h1>Jarno Hänninen</h1>
+      <h2>Software Developer</h2>
+    </div>
   </div>
 </template>
 
@@ -15,16 +18,78 @@ export default {
 </script>
 
 <style scoped>
-
+@home-page-font {
+  font-family: homePageFont;
+  src: ("~/assets/fonts/AllerDisplay.ttf");
+}
 
 .home-page {
-  background-image: url("~assets/images/main-test.jpg");
-  width: 100vw;
-  height: 100vh;
+  position: fixed;
+  background-image: url("~assets/images/main-back.jpg");
+  width: 100%;
+  height: 100%;
   background-position: center;
   background-size: cover;
-  overflow: hidden;
+  text-align: center;
   
 }
+
+.content {
+  align-content: center;
+  position: relative;
+  top: 10%;
+  
+  
+}
+
+h1,
+h2 {
+  font-family: homePageFont;
+  font-size: 450%;
+  color: white;
+  margin: 0px;
+}
+h2 {
+  font-size: 300%;
+  opacity: 0.2;
+}
+
+/* @media screen and (max-width: 1900px){
+  .content {
+    margin-top: 5vw;
+  }
+} */
+/*
+@media screen and (max-width: 500px) {
+  h1,
+  h2 {
+    display: none;
+  }
+}
+
+@media screen and (min-width: 2700px) {
+  
+  h2 {
+    display: none;
+  }
+}
+
+
+@media screen and (max-height: 900px ) {
+  h2 {
+    display: none;
+  }
+}
+
+@media screen and (max-height: 750px ) {
+  .content {
+    top: 1vh;
+  }
+}
+@media screen and (max-height: 600px ) {
+  .content {
+    display: none;
+  }
+}*/
 
 </style>
